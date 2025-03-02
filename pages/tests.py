47d5ee9f0b329bfe00f96aa864cfa518b7c4ad9f
Giorgi Abesadze <44198003+giorgiabes@users.noreply.github.com>
2025-03-02 18:16:18 +0000

    add test

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 
-# Create your tests here.
+class HomepageTest(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+class AboutpageTest(SimpleTestCase):
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/about/")
+        self.assertEqual(response.status_code, 200)
