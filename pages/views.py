f78747c25c788892625268acb6af7c90aad2b629
Giorgi Abesadze <44198003+giorgiabes@users.noreply.github.com>
2025-03-02 17:57:52 +0000

    add about page

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def home_page_view(request):
+    return HttpResponse("Homepage")
+
+def about_page_view(request):
+    context = {"name": "Alice", "age": 21}
+    return render(request, "pages/about.html", context)
